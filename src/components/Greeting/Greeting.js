ea96e2973141cdc6a58028a3b279f0613e77fef9
Tu Nombre <wahandri@wahandri-VirtualBox>
2023-12-31 04:41:42 +0100

    upgrade CSS

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -5,18 +5,18 @@ import "./Greeting.css";
 
 export default function Greeting() {
   const [greeting, setGreeting] = useState(
-    'Hey, my name is <span class="color2">Manuel</span>'
+    'Hola, mi nombre es <span class="color2">Manuel</span>'
   );
   const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setGreeting((prevGreeting) =>
-        prevGreeting === 'Hey, my name is <span class="color2">Manuel</span>'
-          ? 'You can call me <span class="color2">Wahandri</span>'
-          : 'Hey, my name is <span class="color2">Manuel</span>'
+        prevGreeting === 'Hola, mi nombre es <span class="color2">Manuel</span>'
+          ? 'Pero me dicen <span class="color2">Wahandri</span>'
+          : 'Hola, mi nombre es <span class="color2">Manuel</span>'
       );
-    }, 2000);
+    }, 3000);
     return () => clearInterval(interval);
   }, []);
 
@@ -25,9 +25,12 @@ export default function Greeting() {
       <div className="boxMyDescription hello">
         <div className="helloImg">
           <div className="textStart">
-            <h1 dangerouslySetInnerHTML={{ __html: greeting }} />
+            <h1
+              className="fixed"
+              dangerouslySetInnerHTML={{ __html: greeting }}
+            />
             <h1>
-              I am <span className="color2">Web Developer</span>
+              Y soy <span className="color2">Desarrollador Web Fullstack</span>
             </h1>
           </div>
           <div className="divMen">
@@ -37,9 +40,9 @@ export default function Greeting() {
         </div>
         <div className="myDescription">
           <h3>
-            Hello! I'm Manuel García, 30 years old, and I am a{" "}
-            <span className="color2">Full-Stack Web Developer</span> with a
-            strong background from{" "}
+            ¡Hola! Soy Manuel García, tengo 30 años y soy{" "}
+            <span className="color2">Desarrollador Web Full-Stack</span> con una
+            sólida formación en{" "}
             <a
               href="https://codespaceacademy.com/"
               target="_blank"
@@ -47,28 +50,37 @@ export default function Greeting() {
             >
               Codespace Academy
             </a>
-            . My insatiable curiosity for creation and problem-solving drives me
-            to keep learning and applying my knowledge in personal projects.
+            . Mi curiosidad insaciable por la creación y la resolución de
+            problemas me impulsa a seguir aprendiendo y aplicando mis
+            conocimientos en proyectos personales.
           </h3>
           {showMore && ( // Si showMore es true, se muestra el segundo <h3>
             <h3>
-              My positive personality, goal-oriented focus, and ability to work
-              in a team define me. .While my work experience is limited, I
-              believe that my year of immersion in the academy and daily
-              application in personal projects, including creating a free
-              website for a real small business, have been essential to my
-              development. I feel well-prepared to take my skills to the next
-              level and successfully tackle challenges in the real-world web
-              development environment. I am excited to continue growing and
-              contribute significantly to future projects.
+              Mi naturaleza optimista y mi enfoque orientado hacia la
+              consecución de metas, junto con mi habilidad para colaborar
+              efectivamente en equipo, son aspectos clave de mi personalidad.
+              <br /> A pesar de tener una experiencia laboral aún incipiente,
+              considero que mi intensa formación durante un año en la academia y
+              la dedicación constante a proyectos personales han sido
+              fundamentales en mi evolución profesional.
+              <br />
+              <br /> Este periodo incluye el desarrollo de un sitio web sin
+              costo para un pequeño negocio real, una experiencia que ha
+              enriquecido significativamente mi aprendizaje. Me siento
+              plenamente capacitado para elevar mis habilidades a un nivel
+              superior, enfrentando con éxito los retos que presenta el mundo
+              laboral del desarrollo web.
+              <br />
+              <br /> Estoy entusiasmado con la idea de continuar mi crecimiento
+              profesional y aportar de manera notable a los proyectos futuros.
             </h3>
           )}
+          <button className="btnRead" onClick={() => setShowMore(!showMore)}>
+            {" "}
+            {/* Botón para mostrar/ocultar el segundo <h3> */}
+            {showMore ? "Read less" : "Read more"}
+          </button>
         </div>
-        <button className="btnRead" onClick={() => setShowMore(!showMore)}>
-          {" "}
-          {/* Botón para mostrar/ocultar el segundo <h3> */}
-          {showMore ? "Read less" : "Read more"}
-        </button>
       </div>
     </div>
   );
