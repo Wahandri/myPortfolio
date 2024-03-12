99a0ea5f5b422895d28a240df29f9b58f7da62e7
Wahandri <wahandrimasuno@gmail.com>
2024-03-12 22:53:06 +0100

    Updated BtnLink.js

--- src/components/BtnLink/BtnLink.js
+++ src/components/BtnLink/BtnLink.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 import "./BtnLink.css";
-import { Link } from 'react-router-dom';
 
 export default function BtnLink({ to, children, ...props }) {
   return (
-    
-    <Link className='BtnLink' to={to} {...props}>
-        {children}
-    </Link>
-  )
+    <a className='BtnLink' href={to} target="_blank" rel="noopener noreferrer" {...props}>
+      {children}
+    </a>
+  );
 }
